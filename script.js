2a51733a730454ebd612fc70460039bfff8ad8a4
OMAR ALAA <omardev333@gmail.com>
2026-08-01 00:29:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 
 function resetValue(){
-  ctx.filter = 'none';
+  img.style.filter = 'none';
   saturate.value = '100';
   contrast.value = '100';
   brightness.value = '100';
